a486450dd582bd48291e6f97b108be41150e6367
Ksenia <varosianarmen92@gmail.com>
2023-11-09 23:30:02 +0100

    styles added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -17,8 +17,8 @@ const Layout = () => {
           <button
             className='navbar-toggler'
             type='button'
-            data-toggle='collapse'
-            data-target='#navbarNavAltMarkup'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNavAltMarkup'
             aria-controls='navbarNavAltMarkup'
             aria-expanded='false'
             aria-label='Toggle navigation'
